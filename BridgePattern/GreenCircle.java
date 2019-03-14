59f94e4934c0eb966dd52829b3b7aabb2b69e288
tt-anh-eole <tt-anh@eole.co.jp>
2019-03-14 18:42:07 +0900

    Finish Bridge Pattern

---
+++ BridgePattern/GreenCircle.java
@@ -0,0 +1,6 @@
+public class GreenCircle implements DrawAPI {
+    public void drawCircle(int radius, int x, int y) {
+        System.out.print("Drawing circle[ color: green, radius: "
+            + radius + ", x: " + x + ", y: " + y + " ]");
+    }
+}
---
+++ BridgePattern/Shape.java
@@ -0,0 +1,9 @@
+public abstract class Shape {
+    protected DrawAPI drawAPI;
+
+    protected Shape(DrawAPI drawAPI) {
+        this.drawAPI = drawAPI;
+    }
+
+    public abstract void draw();
+}
